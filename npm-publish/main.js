0b72ebf452e2c271a9cc412864e1f55dd91a549c
Adi Mora <adi.mora@gamesys.co.uk>
2016-01-26 09:14:48 +0000

    basic version

---
+++ npm-publish/main.js
@@ -0,0 +1,37 @@
+var fs = require('fs')
+var _ = require('underscore')._
+var winston = require('winston')
+
+var filesizereport = require('./report')
+
+var optimist = require('optimist')
+    .options('output', {
+        alias: 'o'
+        , 'default': 'output/report.json'
+        , describe: 'Output JSON file name.'
+    })
+    .options('path', {
+        alias: 'p'
+        , 'default': 'output'
+        , describe: 'Folder path.'
+    })
+
+var argv = optimist.argv
+var opts = _.extend({}, argv)
+
+winston.remove(winston.transports.Console)
+winston.add(winston.transports.Console, {
+    colorize: true
+    , level: argv.log
+    , handleExceptions: false
+})
+winston.debug('Parsed arguments', argv)
+
+opts.logger = winston
+
+filesizereport(opts, function(err, obj) {
+    if (err) {
+        winston.error(err)
+        process.exit(0)
+    }
+})
